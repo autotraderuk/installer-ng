6adc1c0d9cd503a6b0e20b7781f0978a3492607c
Daniele Testa <daniele.testa@gmail.com>
2015-06-26 14:39:16 +0000

    Igor changes

--- files/scalr-server-cookbooks/scalr-server/recipes/group_nginx_enabled_pre.rb
+++ files/scalr-server-cookbooks/scalr-server/recipes/group_nginx_enabled_pre.rb
@@ -32,9 +32,27 @@ template "#{etc_dir_for node, 'nginx'}/nginx.conf" do
   owner     'root'
   group     'root'
   mode      0644
+  variables :cores => Mixlib::ShellOut.new('cat /proc/cpuinfo | grep processor | wc -l').run_command.stdout.to_i
   helpers do
     include Scalr::PathHelper
     include Scalr::ServiceHelper
   end
   notifies  :restart, 'supervisor_service[nginx]' if service_is_up?(node, 'nginx')
 end
+
+template "#{etc_dir_for node, 'nginx'}/locations.conf" do
+  source    'nginx/locations.conf.erb'
+  owner     'root'
+  group     'root'
+  mode      0644
+  notifies  :restart, 'supervisor_service[nginx]' if service_is_up?(node, 'nginx')
+end
+
+template "#{etc_dir_for node, 'nginx'}/mime.types" do
+  source    'nginx/mime.types.erb'
+  owner     'root'
+  group     'root'
+  mode      0644
+  notifies  :restart, 'supervisor_service[nginx]' if service_is_up?(node, 'nginx')
+end
+
